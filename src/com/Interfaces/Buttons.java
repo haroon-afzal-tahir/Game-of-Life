d02201b6ffc4e2e1b7f6a1a3290e0c14ef93188d
Haroon Tahir <noono14252@outlook.com>
2021-11-18 10:21:34 +0500

    Buttons Interface Created

---
+++ src/com/Interfaces/Buttons.java
@@ -0,0 +1,15 @@
+package com.Interfaces;
+
+public interface Buttons {
+	public void PrintMessage();
+	
+	public void Start();
+	
+	public void Stop();
+	
+	public void Next();
+	
+	public void Restart();
+	
+	public void Lexicon();
+}
