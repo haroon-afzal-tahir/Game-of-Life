26da1065ee69530e7e554af1267dc9017792043a
Haroon Tahir <noono14252@outlook.com>
2021-11-23 22:03:33 +0500

    Interfaces Changed and Some New Getter and Setter Added in Board Class

--- src/com/Interfaces/GetFromBL/DB/LoadStateclass.java
+++ src/com/Interfaces/GetFromBL/DB/LoadStateclass.java
@@ -1,18 +1,12 @@
 package com.Interfaces.GetFromBL.DB;
-import java.io.FileReader;
-import java.io.*;
-import java.io.IOException;
-import java.io.File;
+
+import com.BL.Game;
 
 public class LoadStateclass implements LoadState {
-
-	@Override
-	public void load(int gameID, Cell[][] Grid, int counter, int speed, int score) {
-		// TODO Auto-generated method stub
-		var filename = "file"+ gameID + ".txt";
-		
-		
-	}
-
 	
+	
+	@Override
+	public Game load(int gameID) {
+		return null;
+	}
 }
