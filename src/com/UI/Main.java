c66dbff0f5109a6c12aed78f9ae2ca6f9380f7be
Saqlain <l192326@lhr.nu.edu.pk>
2021-11-24 22:03:14 +0500

    commit2 saqlain

--- src/com/UI/Main.java
+++ src/com/UI/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 public class Main extends Application {
     public static void main(String[] args) {
@@ -32,6 +33,8 @@ public class Main extends Application {
         Scene buttonScene = new Scene(root, 640,600);
         buttonStage.setScene(buttonScene);
         buttonStage.show();
+
+        
 
 
     }
