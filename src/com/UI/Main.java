7459bf929a30a9711e40ed6f0fa977ffb3e2d617
Dell <sabazahra00@gmail.com>
2021-11-24 14:43:15 +0500

    GUI_Buttons with addition

--- src/com/UI/Main.java
+++ src/com/UI/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("GUI_Buttons");
         primaryStage.setScene(new Scene(root, 400, 275));
         primaryStage.show();
     }
