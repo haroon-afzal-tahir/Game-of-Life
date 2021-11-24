feaa25d6b24e095461c42fd6af504cfe652556f4
zarin <81378664+zarintaufique@users.noreply.github.com>
2021-11-24 11:15:06 +0500

    Data members and getters added in Game.java

--- src/com/BL/Game.java
+++ src/com/BL/Game.java
@@ -1,5 +1,16 @@
 package com.BL;
 
 public class Game {
+        private Controls control;
+        private Board board;
 
+
+        public Controls getcontrols() {
+        return control;
+        }
+
+        public Board getBoard() {
+        return board;
+        }
+	  
 }
