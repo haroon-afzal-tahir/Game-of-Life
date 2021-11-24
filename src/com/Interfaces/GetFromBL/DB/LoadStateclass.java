6b0d40629ac9522fc223ba065c2a7f3b0aa25a8d
zarin <l191029@lhr.nu.edu.pk>
2021-11-24 15:42:03 +0500

    Loadgame function changed

--- src/com/Interfaces/GetFromBL/DB/LoadStateclass.java
+++ src/com/Interfaces/GetFromBL/DB/LoadStateclass.java
@@ -6,18 +6,24 @@ public class LoadStateclass implements LoadState {
 	@Override
 	public Game load(int gameID) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 		Game ret_gameobj = new Game();
-		
+		int ch = 0;
 		var filename = "file"+ gameID + ".txt";
+		int generation = 0, counter = 0;
 		try {
-			FileReader fw = new FileReader(filename);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
+			FileReader fr = new FileReader(filename);
+			// read from FileReader till the end of file
+			
+			while ((ch = fr.read()) != -1)
+			    System.out.println((char)ch);
+			
+			// close the file
+			    fr.close();
+			}
+			catch (IOException e) {
+	            e.printStackTrace();
+	        }
 		return ret_gameobj;
 	}
 }
