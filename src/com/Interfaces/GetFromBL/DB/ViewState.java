c49878cb147bff791391652e3f46dffd5ce2b768
zarin <81378664+zarintaufique@users.noreply.github.com>
2021-11-24 21:58:20 +0500

    Update ViewState.java

--- src/com/Interfaces/GetFromBL/DB/ViewState.java
+++ src/com/Interfaces/GetFromBL/DB/ViewState.java
@@ -6,5 +6,5 @@ public interface ViewState {
 	//This shows a list of saved states. 
 	//The user can then select a state from the list and load it.
 	//The name of the selected state will be returned.
-	String view(char[][] list);
+	String view(char[][] list, int size_list);
 }
