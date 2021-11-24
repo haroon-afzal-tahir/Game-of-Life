38e0fefb943e78201a6f85a6c733b857a9cd56ba
Dell <sabazahra00@gmail.com>
2021-11-24 16:50:05 +0500

    GUI_Buttons with additional buttons

--- src/com/UI/Main.java
+++ src/com/UI/Main.java
@@ -1,12 +1,10 @@
 //////////////////////////GUI Buttons///////////////////////////////////////////////
 package com.UI;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
