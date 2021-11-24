a513b2013ab384ae5f4122305891c19de1484836
zarin <81378664+zarintaufique@users.noreply.github.com>
2021-11-24 21:28:43 +0500

    viewstate interface corrected

--- src/com/Interfaces/GetFromBL/DB/ViewState.java
+++ src/com/Interfaces/GetFromBL/DB/ViewState.java
@@ -3,5 +3,8 @@ package com.Interfaces.GetFromBL.DB;
 import com.BL.Game;
 
 public interface ViewState {
-	Game view(int gameId);
+	//This shows a list of saved states. 
+	//The user can then select a state from the list and load it.
+	//The name of the selected state will be returned.
+	String view(char[][] list);
 }
