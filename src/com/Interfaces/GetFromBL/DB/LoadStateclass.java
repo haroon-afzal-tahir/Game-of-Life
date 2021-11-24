e714a4726dc1351f4fabdfe05978c5b7b87a51fa
zarin <81378664+zarintaufique@users.noreply.github.com>
2021-11-24 12:06:06 +0500

    changes in concrete class of load state

--- src/com/Interfaces/GetFromBL/DB/LoadStateclass.java
+++ src/com/Interfaces/GetFromBL/DB/LoadStateclass.java
@@ -3,10 +3,21 @@ package com.Interfaces.GetFromBL.DB;
 import com.BL.Game;
 
 public class LoadStateclass implements LoadState {
-	
-	
 	@Override
 	public Game load(int gameID) {
-		return null;
+		// TODO Auto-generated method stub
+		Game ret_gameobj = new Game();
+		
+		var filename = "file"+ gameID + ".txt";
+		try {
+			FileReader fw = new FileReader(filename);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		return ret_gameobj;
 	}
 }
