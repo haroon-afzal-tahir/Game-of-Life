40fa0c19099a227698f0e221320059845abda238
Saqlain <l192326@lhr.nu.edu.pk>
2021-11-24 20:56:39 +0500

    Main with grid output. Initial grids Interface with 1 dummy alive cell

--- src/com/UI/Main.java
+++ src/com/UI/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-    
+
         GridClass grids = new GridClass();  //creates grid obj
 
 
