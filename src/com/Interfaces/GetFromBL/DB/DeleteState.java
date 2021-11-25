4fe0d1687f813ecd732acfef877f9c88d5b7ca7e
zarin <81378664+zarintaufique@users.noreply.github.com>
2021-11-25 13:27:40 +0500

    DeleteState Interface corrected

--- src/com/Interfaces/GetFromBL/DB/DeleteState.java
+++ src/com/Interfaces/GetFromBL/DB/DeleteState.java
@@ -1,5 +1,9 @@
 package com.Interfaces.GetFromBL.DB;
 
+
+//the Statename will be taken from user in ViewState
+//And then the file will be deleted, and it's name will be deleted from the list of saved states
+//the updated list will be returned to main
 public interface DeleteState {
-	void delete(int gameNo);
+	public char[][] delete(String statename, char[][] list);
 }
