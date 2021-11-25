5a4d2b01f9e984443f968b99eb69c46145ee6c54
zarin <81378664+zarintaufique@users.noreply.github.com>
2021-11-25 13:36:10 +0500

    Add files via upload

---
+++ src/com/Interfaces/GetFromBL/DB/DeleteStateConcrete.java
@@ -0,0 +1,11 @@
+package com.Interfaces.GetFromBL.DB;
+
+class DeleteStateConcrete implements DeleteState{
+
+	@Override
+	public char[][] delete(String statename, char[][] list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
